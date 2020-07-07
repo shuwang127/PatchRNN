3924a0be060ae384cc7122e2e0bea40acf8935f2
Shu W <shuwang127@yahoo.com>
2020-07-06 23:24:05 -0400

    Get the commit message.

--- PatchClassificationRNN.py
+++ PatchClassificationRNN.py
@@ -1,3 +1,93 @@
 '''
     PatchClassificationRNN
 '''
+
+import os
+
+rootPath = './'
+dataPath = rootPath + '/data/'
+sDatPath = dataPath + '/security_patch/'
+pDatPath = dataPath + '/positives/'
+nDatPath = dataPath + '/negatives/'
+
+def main():
+    ReadData()
+    return
+
+def ReadData():
+
+    def ReadCommitMsg(filename):
+        fp = open(filename, encoding='utf-8', errors='ignore')  # get file point.
+        lines = fp.readlines()  # read all lines.
+        #numLines = len(lines)   # get the line number.
+        #print(lines)
+
+        # initialize commit message.
+        commitMsg = []
+        # get the wide range of commit message.
+        for line in lines:
+            if line.startswith('diff --git'):
+                break
+            else:
+                commitMsg.append(line)
+        #print(commitMsg)
+        # process the head of commit message.
+        while (1):
+            headMsg = commitMsg[0]
+            if (headMsg.startswith('From') or headMsg.startswith('Date:') or headMsg.startswith('Subject:')
+                    or headMsg.startswith('commit') or headMsg.startswith('Author:')):
+                commitMsg.pop(0)
+            else:
+                break
+        #print(commitMsg)
+        # process the tail of commit message.
+        dashLines = [i for i in range(len(commitMsg))
+                     if commitMsg[i].startswith('---')]  # finds all lines start with ---.
+        if (len(dashLines)):
+            lnum = dashLines[-1]  # last line number of ---
+            marks = [1 if (' file changed, ' in commitMsg[i] or ' files changed, ' in commitMsg[i]) else 0
+                     for i in range(lnum, len(commitMsg))]
+            if (sum(marks)):
+                for i in reversed(range(lnum, len(commitMsg))):
+                    commitMsg.pop(i)
+        #print(commitMsg)
+
+        #msgShow = ''
+        #for i in range(len(commitMsg)):
+        #    msgShow += commitMsg[i]
+        #print(msgShow)
+
+        return commitMsg
+
+    # read security patch data.
+    for root, ds, fs in os.walk(sDatPath):
+        for file in fs:
+            filename = os.path.join(root, file).replace('\\', '/')
+            #print(filename)
+            #commitMsg = ReadCommitMsg(filename)
+
+    # read security patch data.
+    for root, ds, fs in os.walk(pDatPath):
+        for file in fs:
+            filename = os.path.join(root, file).replace('\\', '/')
+            #print(filename)
+
+    # read security patch data.
+    for root, ds, fs in os.walk(nDatPath):
+        for file in fs:
+            filename = os.path.join(root, file).replace('\\', '/')
+            #print(filename)
+
+    filename = 'F:\_Workspace\PatchClassificationRNN\data\positives/20200401P/adaptivecomputing.torque.da191be852fa3e12e87b846210b949cfe2bc69dc'
+    fp = open(filename, encoding='utf-8', errors='ignore')  # get file point.
+    lines = fp.readlines()  # read all lines.
+    numLines = len(lines)  # get the line number.
+    print(lines)
+
+    atLines = [i for i in range(numLines) if lines[i].startswith('@@ ')]
+    print(atLines)
+
+    return
+
+if __name__ == '__main__':
+    main()
